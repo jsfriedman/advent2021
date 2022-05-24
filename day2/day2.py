49792e3583936991a78ff0f11bac93b4ff9d9833
jsfriedman <friedman.jonny@gmail.com>
2022-05-23 20:53:10 -0400

    solved day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -8,10 +8,25 @@ def part1(instructions):
 
     final_x_position = direction_totals['forward']
     final_depth = direction_totals['down'] - direction_totals['up']
-    print(f"final position (x, depth): ({final_x_position},{final_depth})")
-    print(f"multiplied area is: {final_x_position*final_depth}")
+    print(f"part 1, final position (x, depth): ({final_x_position},{final_depth})")
+    print(f"part 1, multiplied area is: {final_x_position*final_depth}")
+    
+def part2(instructions):
+    x_position, depth_position, aim = [0]*3
+    for instruction in instructions:
+        direction, val = instruction.split(" ")
+        val = int(val)
+        if 'forward' in direction:
+            x_position += val
+            depth_position += aim*val
+        elif 'down' in direction: aim += val
+        elif 'up' in direction: aim -= val
+            
+    print(f"part 2, final position (x, depth): ({x_position},{depth_position})")
+    print(f"part 2, multiplied area is: {x_position*depth_position}")
 
 if __name__ =='__main__':
     with open(os.path.abspath(".\input\day2_input.txt")) as f:
         instructions = f.read().splitlines()
     part1(instructions)
+    part2(instructions)
